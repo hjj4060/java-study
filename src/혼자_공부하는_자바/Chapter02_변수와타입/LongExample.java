93d3122b069888d339706e13f60e56833d6a40f1
hjj4060 <rkdgustn77@naver.com>
2023-11-20 23:19:48 +0900

    혼자_공부하는_자바

---
+++ src/혼자_공부하는_자바/Chapter02_변수와타입/LongExample.java
@@ -0,0 +1,10 @@
+package 혼자_공부하는_자바.Chapter02_변수와타입;
+
+public class LongExample {
+    public static void main(String[] args) {
+        long var1 = 10;
+        long var2 = 20L;
+        //long var3 = 100000000000000000; //컴파일 에러
+        long var4 = 100000000000000000L;
+    }
+}
