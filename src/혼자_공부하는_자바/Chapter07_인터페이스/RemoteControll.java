fd4633d27802494361c5e6ab3986024f685f0847
hjj4060 <rkdgustn77@naver.com>
2023-12-22 00:41:43 +0900

    혼자_공부하는_자바

---
+++ src/혼자_공부하는_자바/Chapter07_인터페이스/RemoteControll.java
@@ -0,0 +1,11 @@
+package 혼자_공부하는_자바.Chapter07_인터페이스;
+
+public interface RemoteControll {
+    public int MAX_VOLUME = 10;
+    public int MIN_VOLUME = 10;
+
+
+    public void turnOn();
+    public void turnOff();
+    public void setMaxVolume(int volume);
+}
---
+++ src/혼자_공부하는_자바/Chapter07_인터페이스/Television.java
@@ -0,0 +1,20 @@
+package 혼자_공부하는_자바.Chapter07_인터페이스;
+
+public class Television implements RemoteControll {
+    private int volume;
+
+    @Override
+    public void turnOn() {
+        System.out.println("tv를 킵니다");
+    }
+
+    @Override
+    public void turnOff() {
+        System.out.println("tv를 끕니다");
+    }
+
+    @Override
+    public void setMaxVolume(int volume) {
+        this.volume = volume;
+    }
+}
